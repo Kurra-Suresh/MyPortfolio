db10e48c4eade8043091897ba8b091157dfa53ad
Kurra-Suresh <sureshkurra49@gmail.com>
2026-07-03 10:37:48 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,7 @@ function About() {
               <section className="page">
                      <h2>About Me</h2>
                      <p>
-                            I am a Frontend Developer with 3.7 years of professional experience, currently working at LTIMindtree since June 2022. I have been involved in building and maintaining scalable, high-performance web applications using React.js and modern JavaScript in a production environment.
+                            I am a Frontend Developer with 4 years of professional experience, currently working at LTIMindtree since June 2022. I have been involved in building and maintaining scalable, high-performance web applications using React.js and modern JavaScript in a production environment.
                      </p>
                      <p>
                             In my role at LTIMindtree, I focus on developing reusable React components, managing application state efficiently, and integrating RESTful APIs to deliver reliable and user-focused solutions. I work closely with designers, backend developers, and stakeholders to translate requirements into responsive and maintainable user interfaces.
